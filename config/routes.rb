ca526d5a74db45e1264f62de9ee00b7fd7fe690f
Carl McMillan <hampizzy@gmail.com>
2011-04-18 15:55:10 +1200

    Fix tests, add help page

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ ShareMachine::Application.routes.draw do
 
   match '/about',   :to => 'pages#about'
   match '/contact', :to => 'pages#contact'
+  match '/help',    :to => 'pages#help'
   
   root :to => 'pages#home'
 
--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -45,5 +45,18 @@ describe PagesController do
                         :content => @base_title + " | Contact")
     end
   end
+  
+  describe "GET 'help'" do
+    it "should be successful" do
+      get 'help'
+      response.should be_success
+    end
+    
+    it "should have the right title" do
+      get 'help'
+      response.should have_selector("title", 
+                        :content => @base_title + " | Help")
+    end
+  end
 
 end
