d6ab34506f8767aef73cca423636d2de34ce5eee
Carl McMillan <hampizzy@gmail.com>
2011-04-18 13:06:19 +1200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ShareMachine::Application.config.secret_token = '4db079bc133a0e7db15749ce8198053532aa1c4179c5fe2e2bcbe67592acfb326473138f087767ef1cee1c6776f4d9a4f730944183db321512c6cae818570fcb'
